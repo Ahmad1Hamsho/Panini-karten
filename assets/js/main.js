fcae89be6a265b9b6f1dcfb28678611dfcb99e88
Ahmad1Hmasho <a1994h.sy@gmail.com>
2020-06-06 00:11:48 +0200

    Cards

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+function printHTML() {
+  const name = document.getElementById("name").value;
+  const profits = document.getElementById("profits").value;
+  const views = document.getElementById("views").value;
+  const photo = document.getElementById("photo").value;
+  const cards = document.getElementById("cards");
+  cards.innerHTML += ` <section style="background-image: url(${photo});" id="card">
+      <div id="name1">${name}</div>
+      <div id="profits1">${profits}</div>
+      <div id="views1">${views}</div>
+    </section>`;
+  document.getElementById("card").style.backgroundImage = `${photo}`;
+}
+function add() {
+  const name = document.getElementById("name").value;
+  const profits = document.getElementById("profits").value;
+  const views = document.getElementById("views").value;
+  const photo = document.getElementById("photo").value;
+
+  class Movie {
+    constructor(namE, profitS, viewS, photO) {
+      this.namE = namE;
+      this.profitS = profitS;
+      this.viewS = viewS;
+      this.photO = photO;
+    }
+  }
+  let movis = [new Movie(name, profits, views, photo)];
+  let json = JSON.stringify(movis);
+  printHTML();
+  console.log(movis);
+  console.log(json);
+}
